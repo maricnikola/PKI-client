f530c6ba2882fe18c3b84e758228d2edf5c6610c
pejinovics <pejinovicstefan@gmail.com>
2024-04-19 15:02:55 +0200

    feat: add nav-bar component

--- pki-client/src/app/layout/layout.module.ts
+++ pki-client/src/app/layout/layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule
